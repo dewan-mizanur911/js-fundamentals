fecd29060ea3c816d1f8b59bdb4dbbfc477ce6dc
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-31 22:25:56 +0600

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+var friendsAge = [11, 13, 17];
+
+var lunchFee = [32, 44 ,56];
+
+var friendName = ['Mamun', 'Riad', 'Sabbir'];
+
+var oddNumbers = [1, 3, 5, 7, 9];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+// console.log(vowels.length);
+
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
