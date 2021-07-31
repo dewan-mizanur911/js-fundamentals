3b23d66001628aee015c3cb91f1e654b1e0e968e
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-01 00:05:14 +0600

    if else condition

---
+++ condition.js
@@ -0,0 +1,13 @@
+var isFoodReady = true;
+if(isFoodReady == true){
+    console.log('ajk khamu re pet fulaya');
+}
+
+var beefPrice = 150;
+var moneyInMyPocket = 120;
+if(beefPrice < moneyInMyPocket){
+    console.log('goru diya khamu');
+}
+else{
+    console.log('vorta ki ase daw dekhi');
+}
