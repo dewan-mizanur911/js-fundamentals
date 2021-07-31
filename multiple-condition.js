3897f241f5c1feb7c3e6567682c82e769368ff62
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-01 00:17:23 +0600

    multiple condition

---
+++ multiple-condition.js
@@ -0,0 +1,23 @@
+var gotJob = false;
+var moneySaved = 250000;
+var hasFlat = true;
+// if(gotJob == true && moneySaved > 200000){
+//     console.log('cholo biya kore feli');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+
+// if(gotJob == true && moneySaved > 200000 && hasFlat == true){
+//     console.log('cholo biya kore feli');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+
+if((gotJob == true && moneySaved > 200000) || hasFlat == true){
+    console.log('cholo biya kore feli');
+}
+else{
+    console.log('tor kopale biya nai');
+}
