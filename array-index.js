a34dbc2adafad1cca80254c0911df563a1660c0f
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-31 22:55:42 +0600

    index get and set of array

---
+++ array-index.js
@@ -0,0 +1,14 @@
+var words = ['books', 'boys', 'girls' ,'school', 'college', 'university', 'job'];
+var bookIndex = words.indexOf('job');
+// console.log(bookIndex);
+
+var numbers = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0];
+// console.log(numbers.indexOf(4));
+// console.log(numbers.indexOf(454));
+// console.log(numbers[3]);
+var numberSpecify = numbers[4];
+// console.log(numberSpecify);
+console.log(numbers);
+numbers[4] = 45;
+numbers[7] = 96;
+console.log(numbers);
