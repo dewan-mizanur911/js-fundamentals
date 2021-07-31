430b403c2b41aad760ffa7f1a8cf55497b5b9320
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-31 23:45:55 +0600

    comparisions and and/or operators

---
+++ comparison.js
@@ -0,0 +1,10 @@
+var myLove = 87;
+var herLove = 99;
+console.log(myLove > herLove);
+console.log(myLove < herLove);
+console.log(myLove == herLove);
+console.log(myLove != herLove);
+
+// and / or operators
+// myLove && herLove
+// myLove || herLove
