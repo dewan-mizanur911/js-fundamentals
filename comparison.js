abbec567a2d6f1677fa70b3d994e5e6d0e2d6f7a
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-01 00:52:28 +0600

    more comparisions added

--- comparison.js
+++ comparison.js
@@ -1,10 +1,13 @@
 var myLove = 87;
 var herLove = 99;
 console.log(myLove > herLove);
+console.log(myLove >= herLove);
 console.log(myLove < herLove);
+console.log(myLove <= herLove);
 console.log(myLove == herLove);
 console.log(myLove != herLove);
 
 // and / or operators
 // myLove && herLove
 // myLove || herLove
+
